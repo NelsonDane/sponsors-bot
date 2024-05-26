25669ad1df92a3fe59476ba699e79f8dc769bd20
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2024-05-26 00:10:44 -0400

    remove name from graphql

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def get_sponsors():
                         node {
                             ... on User {
                                 databaseId
-                                name
                                 url
                             }
                         }
