b1cba747ed9805081346d97fcdbe63df7213958c
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2024-10-26 16:48:32 -0400

    only print if former sponsor

--- gh.py
+++ gh.py
@@ -33,8 +33,9 @@ def update_sponsors(db: EdgeDB):
     gh_sponsors = get_sponsors()
     for user in users:
         if not any(sponsor["node"]["databaseId"] == user.gh_id for sponsor in gh_sponsors):
+            if user.is_currently_sponsoring:
+                print(f"User {user.gh_username} is no longer sponsoring")
             db.update_sponsor_is_currently_sponsoring(user.gh_id, False)
-            print(f"User {user.gh_username} is no longer sponsoring")
     for sponsor in gh_sponsors:
         sponsor = sponsor["node"]
         if db.get_sponsor_by_gh_id(sponsor["databaseId"]):
